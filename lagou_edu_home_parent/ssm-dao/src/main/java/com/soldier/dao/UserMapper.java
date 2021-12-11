5db0de9585f68b16246c90c8b53e1f47aec93401
soldier <2538975920@qq.com>
2021-12-11 21:29:49 +0800

    add GitMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
@@ -52,5 +52,14 @@ public interface UserMapper {
      */
     public List<Resource> findResourceByRoleId(@Param("ids") List<Integer> ids);
 
-    public List<Resource> findResourceById(@Param("ids") List<Integer> ids)
+    public List<Resource> findResourceById(@Param("ids") List<Integer> ids);
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
+    public void test8();
 }
