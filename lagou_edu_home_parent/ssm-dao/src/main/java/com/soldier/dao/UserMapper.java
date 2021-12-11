ff51fa9769fee438b99244038e38dd8f79a5b6ee
寒露凝珠 <88575259+CNsoldier@users.noreply.github.com>
2021-12-11 21:29:27 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
@@ -53,4 +53,13 @@ public interface UserMapper {
     public List<Resource> findResourceByRoleId(@Param("ids") List<Integer> ids);
 
     public List<Resource> findResourceById(@Param("ids") List<Integer> ids);
+    
+    public void test11();
+    public void test21();
+    public void test31();
+    public void test41();
+    public void test51();
+    public void test61();
+    public void test71();
+    public void test81();
 }
