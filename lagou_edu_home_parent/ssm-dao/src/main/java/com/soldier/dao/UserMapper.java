ec3e3d8198ff88fcb438df339692e76500802b39
soldier <2538975920@qq.com>
2021-12-14 15:06:07 +0800

    first commit

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/soldier/dao/UserMapper.java
@@ -52,22 +52,4 @@ public interface UserMapper {
      */
     public List<Resource> findResourceByRoleId(@Param("ids") List<Integer> ids);
 
-    public List<Resource> findResourceById(@Param("ids") List<Integer> ids);
-
-    public void test11();
-    public void test21();
-    public void test31();
-    public void test41();
-    public void test51();
-    public void test61();
-    public void test71();
-    public void test81();
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
-    public void test8();
 }
